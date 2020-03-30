c56ceb6f232f008d4411a3a06320103ea2e268da
Ishita Chourasia <ishitach@users.noreply.github.com>
2020-03-30 18:12:25 +0530

    Create LIS.java

---
+++ dynamic_programming/LIS.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class LIS{
+
+	public static void main(String[] args){
+
+		String one = “abcdaf”;
+		String two = “acbcf”;
+		System.out.println(lis(one,two));
+}
+	
+	private static int lis(String one, String two){
+		
+		int[][] temp = new int[one.length()+1][two.length()+1];
+		int resp =0;
+		for(int i=1;i<=one.length();i++){
+			for(int j=1;j<=two.length();j++){
+				if(one.charAt(i) == two.charAt(j)){
+					temp[i][j] = temp[i-1][j-1]+1;
+         }
+				else{
+					temp[i][j] = Math.max(temp[i-1][j],temp[i][j-1]);
+        }
+      }
+    }
+    return temp[one.length()][two.length()];
+  }
+}
